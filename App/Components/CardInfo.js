5008662de8b14ea544c4b5772d431ea08edcb42b
Ghan <ghanpatel@gmail.com>
2015-05-03 22:57:15 -0400

    working on cardinfo screen, setting up webviews

--- App/Components/CardInfo.js
+++ App/Components/CardInfo.js
@@ -19,11 +19,6 @@ var styles = StyleSheet.create({
     width: 250,
     height: 500
   },
-  buttonText: {
-    fontSize: 24,
-    color: 'white',
-    alignSelf: 'center'
-  },
   header: {
     fontSize: 18,
     color: '#111',
@@ -33,11 +28,65 @@ var styles = StyleSheet.create({
     fontSize: 14,
     color: '#111',
     alignSelf: 'center'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#111',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 45,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    marginTop: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
   }
 });
 
 class CardInfo extends React.Component{
+
+  openPage(url, label){
+    this.props.navigator.push({
+      component: Web_View,
+      title: {label},
+      passProps: {url}
+    });
+  }
+
   render() {
+    console.log(this.props.card);
+
+    var buttons = [
+      {
+        label: "Biddy Tarot",
+        url: this.props.card.biddy_link
+      },
+      {
+        label: "Keen Tarot",
+        url: this.props.card.keen_link
+      },
+      {
+        label: "Learn Tarot",
+        url: this.props.card.learn_tarot_link
+      }
+    ];
+
+    var externalButtons = buttons.map((item, index) => {
+      return (
+        <TouchableHighlight
+          style={styles.button}
+          onPress={ () => {this.openPage(item.url, item.label)} }
+          underlayColor="white">
+          <Text style={styles.buttonText}>{item.label}</Text>
+        </TouchableHighlight>
+      )
+    });
+
     return (
       <ScrollView style={styles.container}>
         <Image
@@ -47,6 +96,9 @@ class CardInfo extends React.Component{
           style={styles.header}>{this.props.card.full_name}</Text>
         <Text
           style={styles.description}>{this.props.card.description} </Text>
+        <Text
+          style={styles.external}>External Links: </Text>
+        {externalButtons}
       </ScrollView>
     )
   }
